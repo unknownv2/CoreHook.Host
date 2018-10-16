ce7d03a31d417d78719df5c0c9326003f466645b
Thierry <unknownv2@users.noreply.github.com>
2018-10-15 22:16:36 -0400

    test: Add more delegate functions from the simple Calculator dotnet assembly to be test function. Also add a test for the host app domain unloading and host stopping.

--- Windows/tests/corerundll-test/assembly-test.cpp
+++ Windows/tests/corerundll-test/assembly-test.cpp
@@ -3,8 +3,21 @@
 
 TEST(TestCoreCLRHost, TestDotNetAssemblyExecution) {
 
-    PCWSTR dotnetAssemblyName = L"Calculator.dll";
-    static const wchar_t *coreCLRInstallDirectory = L"%programfiles%\\dotnet\\shared\\Microsoft.NETCore.App\\2.1.5";
+    // Assembly file name for getting base library path 
+    // that is given to the .NET Core host when starting it
+    PCWSTR dotnetAssemblyName =     L"Calculator.dll";
+    // Assembly name used for delegate resolving
+    PCWSTR assemblyName =           L"Calculator";
+    // Class name used for delegate resolving
+    PCWSTR assemblyType =           L"Calculator.Calculator";
+    // Static class function names used for delegate resolving
+    PCWSTR assemblyEntryAdd =       L"Add";
+    PCWSTR assemblyEntrySubtract =  L"Subtract";
+    PCWSTR assemblyEntryMultiply =  L"Multiply";
+    PCWSTR assemblyEntryDivide =    L"Divide";
+
+    PCWSTR coreCLRInstallDirectory
+        = L"%programfiles%\\dotnet\\shared\\Microsoft.NETCore.App\\2.1.5";
 
     BinaryLoaderArgs binaryLoaderArgs = { 0 };
     AssemblyFunctionCall assemblyFunctionCall = { 0 };
@@ -20,18 +33,61 @@ TEST(TestCoreCLRHost, TestDotNetAssemblyExecution) {
     EXPECT_EQ(NOERROR, StartCoreCLR(&binaryLoaderArgs));
 
     typedef int (STDMETHODCALLTYPE AddMethodFp)(const int a, const int b);
-    AddMethodFp *pfnAddDelegate = NULL;
+    AddMethodFp *pfnMethodDelegate = NULL;
 
-    EXPECT_EQ(NO_ERROR,
+    // Test the 'int Add(int a, int b) => a + b;' method
+    EXPECT_EQ(NOERROR,
         CreateAssemblyDelegate(
-            L"Calculator",
-            L"Calculator.Calculator",
-            L"Add",
-            reinterpret_cast<PVOID*>(&pfnAddDelegate)));
+            assemblyName,
+            assemblyType,
+            assemblyEntryAdd,
+            reinterpret_cast<PVOID*>(&pfnMethodDelegate)));
 
+    // Our simple test values
     const int integerA = 1;
     const int integerB = 2;
-    const int integerResult = integerA + integerB;
+    const int integerAddResult = integerA + integerB;
 
-    EXPECT_EQ(integerResult, pfnAddDelegate(integerA, integerB));
+    EXPECT_EQ(integerAddResult, pfnMethodDelegate(integerA, integerB));
+
+    // Test the 'int Subtract(int a, int b) => b - a;' method
+    pfnMethodDelegate = NULL;
+
+    EXPECT_EQ(NOERROR,
+        CreateAssemblyDelegate(
+            assemblyName,
+            assemblyType,
+            assemblyEntrySubtract,
+            reinterpret_cast<PVOID*>(&pfnMethodDelegate)));
+
+    const int integerSubResult = integerB - integerA;
+    EXPECT_EQ(integerSubResult, pfnMethodDelegate(integerA, integerB));
+
+    // Test the 'int Multiply(int a, int b) => a * b;' method
+    pfnMethodDelegate = NULL;
+
+    EXPECT_EQ(NOERROR,
+        CreateAssemblyDelegate(
+            assemblyName,
+            assemblyType,
+            assemblyEntryMultiply,
+            reinterpret_cast<PVOID*>(&pfnMethodDelegate)));
+
+    const int integerMulResult = integerA * integerB;
+    EXPECT_EQ(integerMulResult, pfnMethodDelegate(integerA, integerB));
+
+    // Test the 'int Divide(int a, int b) => a / b;' method
+    pfnMethodDelegate = NULL;
+
+    EXPECT_EQ(NOERROR,
+        CreateAssemblyDelegate(
+            assemblyName,
+            assemblyType,
+            assemblyEntryDivide,
+            reinterpret_cast<PVOID*>(&pfnMethodDelegate)));
+
+    const int integerDivResult = integerA / integerB;
+    EXPECT_EQ(integerDivResult, pfnMethodDelegate(integerA, integerB));
+
+    EXPECT_EQ(NOERROR, UnloadRunTime());
 }
