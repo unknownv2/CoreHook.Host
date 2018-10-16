bc9b3387289a52b3cd1cdb8b429799160623b34c
Thierry <unknownv2@users.noreply.github.com>
2018-10-15 21:38:42 -0400

    test: Add initial dotnet assembly test that starts CoreCLR and calls an Add function located in a .NET Calculator

--- Windows/tests/corerundll-test/assembly-test.cpp
+++ Windows/tests/corerundll-test/assembly-test.cpp
@@ -1,6 +1,37 @@
 #include "pch.h"
+#include "../../src/corerundll/corerundll.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCoreCLRHost, TestDotNetAssemblyExecution) {
+
+    PCWSTR dotnetAssemblyName = L"Calculator.dll";
+    static const wchar_t *coreCLRInstallDirectory = L"%programfiles%\\dotnet\\shared\\Microsoft.NETCore.App\\2.1.5";
+
+    BinaryLoaderArgs binaryLoaderArgs = { 0 };
+    AssemblyFunctionCall assemblyFunctionCall = { 0 };
+    binaryLoaderArgs.Verbose = true;
+    binaryLoaderArgs.WaitForDebugger = false;
+    wcscpy_s(binaryLoaderArgs.BinaryFilePath, MAX_PATH, dotnetAssemblyName);
+
+    WCHAR coreCLRInstallPath[MAX_PATH];
+    ::ExpandEnvironmentStringsW(coreCLRInstallDirectory, coreCLRInstallPath, MAX_PATH);
+
+    wcscpy_s(binaryLoaderArgs.CoreRootPath, MAX_PATH, coreCLRInstallPath);
+
+    EXPECT_EQ(NOERROR, StartCoreCLR(&binaryLoaderArgs));
+
+    typedef int (STDMETHODCALLTYPE AddMethodFp)(const int a, const int b);
+    AddMethodFp *pfnAddDelegate = NULL;
+
+    EXPECT_EQ(NO_ERROR,
+        CreateAssemblyDelegate(
+            L"Calculator",
+            L"Calculator.Calculator",
+            L"Add",
+            reinterpret_cast<PVOID*>(&pfnAddDelegate)));
+
+    const int integerA = 1;
+    const int integerB = 2;
+    const int integerResult = integerA + integerB;
+
+    EXPECT_EQ(integerResult, pfnAddDelegate(integerA, integerB));
 }
