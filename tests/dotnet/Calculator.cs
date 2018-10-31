8b57b9508af7b52c1d0423ece05d962f27ee04ec
Thierry <unknownv2@users.noreply.github.com>
2018-10-31 17:37:28 -0400

    doc: Add more comments to Calculator test class and fix typos

--- tests/dotnet/Calculator.cs
+++ tests/dotnet/Calculator.cs
@@ -5,37 +5,37 @@ namespace Calculator
     public class Calculator
     {
         /// <summary>
-        /// Method called by the CoreHook host to initialize the hooking library.
+        /// Called by the CoreHook native host module to initialize the plugin with user-defined arguments.
         /// </summary>
-        /// <param name="remoteParameters">A pointer containing information about the hooking library.</param>
+        /// <param name="remoteParameters">A pointer containing information about the plugin to initialize.</param>
         public static void Load(IntPtr remoteParameters) => System.Diagnostics.Debug.WriteLine($"The pointer parameter was {remoteParameters.ToInt64():X16}.");
         /// <summary>
         /// Add <paramref name="a"/> to <paramref name="b"/>.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns><paramref name="a"/> plus <paramref name="b"/>.</returns>
+        /// <param name="a">An integer value</param>
+        /// <param name="b">An integer value</param>
+        /// <returns>The sum of <paramref name="a"/> and <paramref name="b"/>.</returns>
         public static int Add(int a, int b) => a + b;
         /// <summary>
         /// Subtract <paramref name="a"/> from <paramref name="b"/>.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns><paramref name="b"/> minus <paramref name="a"/>.</returns>
+        /// <param name="a">An integer value</param>
+        /// <param name="b">An integer value</param>
+        /// <returns>The difference of <paramref name="b"/> and <paramref name="a"/>.</returns>
         public static int Subtract(int a, int b) => b - a;
         /// <summary>
-        /// Mulitply <paramref name="a"/> by <paramref name="b"/>.
+        /// Multiply <paramref name="a"/> by <paramref name="b"/>.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="a">The multiplicand</param>
+        /// <param name="b">The multiplier</param>
         /// <returns><paramref name="a"/> times <paramref name="b"/>.</returns>
         public static int Multiply(int a, int b) => a * b;
         /// <summary>
         /// Divide <paramref name="a"/> by <paramref name="b"/>.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns><paramref name="a"/> divied by <paramref name="b"/>.</returns>
+        /// <param name="a">The dividend</param>
+        /// <param name="b">The divisor</param>
+        /// <returns>The quotient of <paramref name="a"/> and <paramref name="b"/>.</returns>
         public static int Divide(int a, int b) => a / b;
     }
 }
