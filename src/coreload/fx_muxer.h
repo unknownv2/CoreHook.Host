d16369481bc2ce66b3989dca58ccbc4e511b6c7a
Thierry <thierry44@gmail.com>
2018-11-12 22:01:58 -0500

    Remove unused functions from the fx_muxer class

--- src/coreload/fx_muxer.h
+++ src/coreload/fx_muxer.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "libhost.h"
 namespace coreload {
-    // Copyright (c) .NET Foundation and contributors. All rights reserved.
-    // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
     class corehost_init_t;
     class runtime_config_t;
@@ -10,27 +8,11 @@ namespace coreload {
     struct fx_ver_t;
     struct host_startup_info_t;
 
-
-
-    /*int execute_app(
-        const pal::string_t& impl_dll_dir,
-        corehost_init_t* init,
-        const int argc,
-        const pal::char_t* argv[]);
-
-    int execute_host_command(
-        const pal::string_t& impl_dll_dir,
-        corehost_init_t* init,
-        const int argc,
-        const pal::char_t* argv[],
-        pal::char_t result_buffer[],
-        int32_t buffer_size,
-        int32_t* required_buffer_size);*/
     int read_config(
         fx_definition_t& app,
         const pal::string_t& app_candidate,
-        pal::string_t& runtime_config
-    );
+        pal::string_t& runtime_config);
+
     class fx_muxer_t
     {
     public:
@@ -46,56 +28,12 @@ namespace coreload {
             pal::char_t out_buffer[],
             int32_t buffer_size,
             int32_t* required_buffer_size);
-        static int execute(
-            const pal::string_t host_command,
-            const int argc,
-            const pal::char_t* argv[],
-            const host_startup_info_t& host_info,
-            pal::char_t result_buffer[],
-            int32_t buffer_size,
-            int32_t* required_buffer_size);
-        static bool resolve_sdk_dotnet_path(const pal::string_t& dotnet_root, const pal::string_t& cwd, pal::string_t* cli_sdk);
     private:
-        static int parse_args(
-            const host_startup_info_t& host_info,
-            int argoff,
-            int argc,
-            const pal::char_t* argv[],
-            bool exec_mode,
-            host_mode_t mode,
-            int* new_argoff,
-            pal::string_t& app_candidate,
-            opt_map_t& opts);
-        static int handle_exec(
-            const host_startup_info_t& host_info,
-            const pal::string_t& app_candidate,
-            const opt_map_t& opts,
-            int argc,
-            const pal::char_t* argv[],
-            int argoff,
-            host_mode_t mode);
-        static int handle_exec_host_command(
-            const pal::string_t& host_command,
-            const host_startup_info_t& host_info,
-            const pal::string_t& app_candidate,
-            const opt_map_t& opts,
-            int argc,
-            const pal::char_t* argv[],
-            int argoff,
-            host_mode_t mode,
-            pal::char_t result_buffer[],
-            int32_t buffer_size,
-            int32_t* required_buffer_size);
-        static int handle_cli(
-            const host_startup_info_t& host_info,
-            int argc,
-            const pal::char_t* argv[]);
+
         static std::vector<host_option> get_known_opts(
             bool exec_mode,
             host_mode_t mode,
-            bool get_all_options = false);
-   
-
+            bool get_all_options = false); 
         static bool resolve_hostpolicy_dir(
             host_mode_t mode,
             const pal::string_t& dotnet_root,
@@ -116,7 +54,6 @@ namespace coreload {
             const runtime_config_t& config,
             const pal::string_t& dotnet_dir,
             const pal::string_t& specified_fx_version);
-        static void muxer_usage(bool is_sdk_present);
     };
 
 }
