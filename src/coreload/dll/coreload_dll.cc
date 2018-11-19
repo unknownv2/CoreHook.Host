26527d68cf6a07300a65b9f7efb00815f7d5d084
Thierry <thierry44@gmail.com>
2018-11-18 21:10:02 -0500

    Add default case handling for the coreload dll entry point

--- src/coreload/dll/coreload_dll.cc
+++ src/coreload/dll/coreload_dll.cc
@@ -106,6 +106,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
         case DLL_PROCESS_DETACH:
+        default:
             break;
     }
     return TRUE;
